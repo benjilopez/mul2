5de301256bcc2d191c7bf2c2e1d92f3ff36f0409
Denny Hörtz <dino368@gmail.com>
2015-11-03 21:05:28 +0100

    Übung 2 - Aufgabe 5a (Bonus) -> deliver a text file with needed read time

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var fs = require('fs');
 
 /**
  * Allow static request to /public folder
@@ -13,6 +14,20 @@ app.use('/public', express.static(path.join(__dirname, '..', '/public')));
 app.get("/time", function(req, res){
     res.setHeader('content-type', 'text/plain');
     res.send(Date.now().toString());
+});
+
+/**
+ * Get a sample text file with read time
+ */
+app.get("/file", function(req, res){
+
+    var startedTime = Date.now();
+
+    fs.readFile(path.join(__dirname, '..', '/public/files/sample.txt'), 'utf8', function(err, contents) {
+
+        res.setHeader('content-type', 'text/plain');
+        res.send(contents + "\nFile was read in " + (Date.now() - startedTime) + "ms.")
+    });
 });
 
 /**
