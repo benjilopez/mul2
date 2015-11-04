8cf4d951754464b8f8c03b8d335b7f26c672aeef
Benjamin Lopez <benjilopez@gmail.com>
2015-11-04 09:00:33 +0100

    Using memoisation pattern when reading files

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var fs = require('fs');
+var fileContent = undefined;
 
 /**
  * Allow static request to /public folder
@@ -17,17 +18,21 @@ app.get("/time", function(req, res){
 });
 
 /**
- * Get a sample text file with read time
+ * Get a sample text file with read time - Using Memoisation Pattern
  */
 app.get("/file", function(req, res){
 
     var startedTime = Date.now();
-
-    fs.readFile(path.join(__dirname, '..', '/public/files/sample.txt'), 'utf8', function(err, contents) {
-
+    if(!fileContent) {
+        fs.readFile(path.join(__dirname, '..', '/public/files/sample.txt'), 'utf8', function (err, contents) {
+            fileContent = contents;
+            res.setHeader('content-type', 'text/plain');
+            res.send(fileContent + "\nFile was read in " + (Date.now() - startedTime) + "ms.");
+        });
+    } else {
         res.setHeader('content-type', 'text/plain');
-        res.send(contents + "\nFile was read in " + (Date.now() - startedTime) + "ms.")
-    });
+        res.send(fileContent + "\nFile was read in " + (Date.now() - startedTime) + "ms.");
+    }
 });
 
 /**
