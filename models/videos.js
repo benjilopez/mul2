2b54b8c0b7cc06621eace97ecb0d790728937cff
Toni <toni.kluth@gmail.com>
2015-12-14 17:05:13 +0100

    finished 1a, VideoSchema added to project

--- models/videos.js
+++ models/videos.js
@@ -2,11 +2,17 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var TweetSchema = new Schema({
-    message:  { type: String, required: true},
-    creator:  { type: Schema.Types.ObjectId, ref: 'User'}
+var VideoSchema = new Schema({
+    _id: {type: String, default: Schema.Types.ObjectId},
+    title: {type: String, required: true},
+    description: {type: String, default: ''},
+    src: {type: String, required: true},
+    length: {type: Number, required: true},
+    timestamp: {type: Number, default: Date.now()},
+    playcount: {type: Number, default: 0},
+    ranking: {type: Number, default: 0}
 }, {
     timestamps: {createdAt: 'timestamp'}
 });
 
-module.exports = mongoose.model('Tweet', TweetSchema);
+module.exports = mongoose.model('Video', VideoSchema);
